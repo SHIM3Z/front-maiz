0559f65b17d4906917c58db68648c123dd14cd91
Paolo Siancas <shim3z151002@gmail.com>
2024-11-12 17:19:13 -0500

    cambios importantes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./componentes/navbar/navbar.component";
+import { InicioComponent } from "./pages/inicio/inicio.component";
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'front-maiz';
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { ImagenComponent } from './pages/imagen/imagen.component';
+import { TiempoRealComponent } from './pages/tiempo-real/tiempo-real.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/inicio/inicio.component').then((m) =>m.InicioComponent),
+
+  },
+];
