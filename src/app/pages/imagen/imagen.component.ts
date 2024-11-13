e3767f113c6286788e1b1b9a2725bd7622f01f8a
Paolo Siancas <shim3z151002@gmail.com>
2024-11-13 18:43:10 -0500

    codigo prueba para cargar y visualizar imagenes

--- src/app/pages/imagen/imagen.component.ts
+++ src/app/pages/imagen/imagen.component.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,4 +10,31 @@ import { Component } from '@angular/core';
   styleUrl: './imagen.component.css'
 })
 export class ImagenComponent {
+  imageBase64: string | null = null;
+  selectedFile: File | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  onFileChange(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  uploadImage() {
+    if (!this.selectedFile) return;
+
+    const formData = new FormData();
+    formData.append('file', this.selectedFile);
+
+    // Llamada al backend para procesar la imagen
+    this.http.post<any>('http://127.0.0.1:8000/process_image/', formData)
+      .subscribe({
+        next: (response) => {
+          // Obtener la imagen en base64 de la respuesta
+          this.imageBase64 = response.image;
+        },
+        error: (err) => {
+          console.error('Error al subir la imagen:', err);
+        }
+      });
+  }
 }
