f3142a2af9282bc3ec20a778b761f8458ba392e0
Paolo Siancas <shim3z151002@gmail.com>
2024-11-18 17:10:29 -0500

    componente prec tiempo real

---
+++ src/app/pages/tiempo-real/tiempores/tiempores.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tiempores',
+  standalone: true,
+  imports: [],
+  templateUrl: './tiempores.component.html',
+  styleUrl: './tiempores.component.css',
+})
+export class TiemporesComponent { }
