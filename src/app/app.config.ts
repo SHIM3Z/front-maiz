dba817088775124fcb54d11f8a90d366a05cf864
SHIM3Z <shim3z151002@gmail.com>
2025-11-28 12:55:48 -0500

    cambios

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,15 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
-    provideToastr({timeOut: 3000, progressBar: true, tapToDismiss: true}),
+    provideToastr({ timeOut: 3000, progressBar: true, tapToDismiss: true }),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withViewTransitions()),
-    provideHttpClient()]
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    provideHttpClient(),
+  ],
 };
